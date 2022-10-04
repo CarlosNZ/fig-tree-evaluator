81ad39ab53f6b38bc1dbae1181fec04793e437ea
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-10-04 17:16:18 +1300

    Add split operator and tests

---
+++ src/operators/split.ts
@@ -0,0 +1,60 @@
+import { evaluateArray } from './_operatorUtils'
+import { EvaluatorConfig, OperatorObject, EvaluatorNode, CombinedOperatorNode } from '../types'
+import { BasicExtendedNode } from './logicalAnd'
+
+const requiredProperties = ['value'] as const
+const operatorAliases = ['split', 'arraySplit']
+const propertyAliases = {
+  string: 'value',
+  trim: 'trimWhiteSpace',
+  trimWhitespace: 'trimWhiteSpace',
+  removeTrailing: 'excludeTrailing',
+  excludeTrailingDelimiter: 'excludeTrailing',
+}
+
+export type SplitNode = {
+  [key in typeof requiredProperties[number]]: EvaluatorNode
+} & BasicExtendedNode & {
+    delimiter?: EvaluatorNode
+    trimWhiteSpace: EvaluatorNode
+    excludeTrailing: EvaluatorNode
+  }
+
+const DEFAULT_DELIMITER = ' '
+
+const evaluate = async (expression: SplitNode, config: EvaluatorConfig): Promise<string[]> => {
+  const [value, delimiter, trimWhiteSpace, excludeTrailing] = (await evaluateArray(
+    [
+      expression.value,
+      expression.delimiter ?? DEFAULT_DELIMITER,
+      expression.trimWhiteSpace ?? true,
+      expression.excludeTrailing ?? true,
+    ],
+    config
+  )) as [string, string, boolean, boolean]
+
+  config.typeChecker(
+    { name: 'value', value: value, expectedType: 'string' },
+    { name: 'delimiter', value: delimiter, expectedType: 'string' },
+    { name: 'trimWhiteSpace', value: trimWhiteSpace, expectedType: 'boolean' },
+    { name: 'excludeTrailing', value: excludeTrailing, expectedType: 'boolean' }
+  )
+
+  let splitValues = value.split(delimiter)
+  if (trimWhiteSpace) splitValues = splitValues.map((val) => val.trim())
+  if (excludeTrailing && splitValues[splitValues.length - 1] === '') splitValues.pop()
+  return splitValues
+}
+
+const parseChildren = (expression: CombinedOperatorNode): SplitNode => {
+  const [value, delimiter = DEFAULT_DELIMITER] = expression.children as EvaluatorNode[]
+  return { ...expression, value, delimiter }
+}
+
+export const SPLIT: OperatorObject = {
+  requiredProperties,
+  operatorAliases,
+  propertyAliases,
+  evaluate,
+  parseChildren,
+}
